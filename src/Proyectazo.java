3141efb7d870bde01937909efd1be42ec8907cda
Fiorella <fiorellamoragon@gmail.com>
2024-04-25 19:13:32 +0200

    Versión 1

---
+++ src/Proyectazo.java
@@ -0,0 +1,10 @@
+
+public class Proyectazo {
+
+	public static void main(String[] args) {
+		// A continuación, un pedazo de programa revolucionario para el mundo de la informática
+		System.out.println("Hola Mundo");
+
+	}
+
+}
