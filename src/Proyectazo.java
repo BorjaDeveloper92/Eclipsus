19fca6488c9f7fd5ca69d59988281a7d0937d1d7
Fiorella <fiorellamoragon@gmail.com>
2024-04-25 19:16:12 +0200

    Acabo de hacer una modificacion

--- src/Proyectazo.java
+++ src/Proyectazo.java
@@ -4,6 +4,7 @@ public class Proyectazo {
 	public static void main(String[] args) {
 		// A continuación, un pedazo de programa revolucionario para el mundo de la informática
 		System.out.println("Hola Mundo");
+		System.out.println();
 
 	}
 
